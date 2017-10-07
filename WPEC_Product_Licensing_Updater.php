c9b022d33bb7b07b16f7fb996dbd33fff6b515be
Joldis Mihai Alexandru <jmihaialexandru@gmail.com>
2017-10-07 16:43:14 +0300

    Bump updater version

--- WPEC_Product_Licensing_Updater.php
+++ WPEC_Product_Licensing_Updater.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * Allows plugins to use their own update API.
  *
  * @author Easy Digital Downloads
- * @version 1.6.13
+ * @version 1.6.15
  */
 class WPEC_Product_Licensing_Updater {
 
@@ -41,13 +41,10 @@ class WPEC_Product_Licensing_Updater {
 		$this->version     = $_api_data['version'];
 		$this->wp_override = isset( $_api_data['wp_override'] ) ? (bool) $_api_data['wp_override'] : false;
 		$this->beta        = ! empty( $this->api_data['beta'] ) ? true : false;
-		$this->cache_key   = md5( serialize( $this->slug . $this->api_data['license'] . $this->beta ) );
-
-		$edd_plugin_data[ $this->slug ] = $this->api_data;
+		$this->cache_key   = 'wpec_sl_' . md5( serialize( $this->slug . $this->api_data['license'] . $this->beta ) );
 
 		// Set up hooks.
 		$this->init();
-
 	}
 
 	/**
@@ -80,7 +77,7 @@ class WPEC_Product_Licensing_Updater {
 
 		$license = get_option( 'wpec_product_' . $this->api_data['item_id'] . '_license_active' );
 		if( ( ! is_object( $license ) || 'valid' !== $license->license ) && empty( $showed_imissing_key_message[ $this->name ] ) ) {
-			echo '&nbsp;<strong><a href="' . esc_url( admin_url( 'index.php?page=wpsc-upgrades' ) ) . '">' . __( 'Enter valid license key for automatic updates.', 'wp-e-commerce' ) . '</a></strong>';
+			echo '&nbsp;<strong><a href="' . esc_url( admin_url( 'index.php?page=wpsc-upgrades' ) ) . '">' . __( 'Enter valid license key for automatic updates.', 'wpec-licensing' ) . '</a></strong>';
 			$showed_imissing_key_message[ $this->name ] = true;
 		}
 	}
@@ -128,10 +125,6 @@ class WPEC_Product_Licensing_Updater {
 			if ( version_compare( $this->version, $version_info->new_version, '<' ) ) {
 
 				$_transient_data->response[ $this->name ] = $version_info;
-
-			} else {
-
-				$_transient_data->response[ $this->name ][ 'slug' ] = $this->slug;
 
 			}
 
@@ -200,9 +193,7 @@ class WPEC_Product_Licensing_Updater {
 			set_site_transient( 'update_plugins', $update_cache );
 
 		} else {
-
 			$version_info = $update_cache->response[ $this->name ];
-
 		}
 
 		// Restore our filter
@@ -281,10 +272,10 @@ class WPEC_Product_Licensing_Updater {
 		$cache_key = 'wpec_lic_api_request_' . md5( serialize( $this->slug . $this->api_data['license'] . $this->beta ) );
 
 		// Get the transient where we store the api request for this plugin for 24 hours
-		$edd_api_request_transient = $this->get_cached_version_info( $cache_key );
+		$wpec_api_request_transient = $this->get_cached_version_info( $cache_key );
 
 		//If we have no transient-saved value, run the API, set a fresh transient with the API value, and return that value too right now.
-		if ( empty( $edd_api_request_transient ) ){
+		if ( empty( $wpec_api_request_transient ) ) {
 
 			$api_response = $this->api_request( 'plugin_information', $to_send );
 
@@ -296,7 +287,7 @@ class WPEC_Product_Licensing_Updater {
 			}
 
 		} else {
-			$_data = $edd_api_request_transient;
+			$_data = $wpec_api_request_transient;
 		}
 
 		// Convert sections into an associative array, since we're getting an object, but Core expects an array.
@@ -444,7 +435,6 @@ class WPEC_Product_Licensing_Updater {
 				$version_info = json_decode( wp_remote_retrieve_body( $request ) );
 			}
 
-
 			if ( ! empty( $version_info ) && isset( $version_info->sections ) ) {
 				$version_info->sections = maybe_unserialize( $version_info->sections );
 			} else {
@@ -495,7 +485,7 @@ class WPEC_Product_Licensing_Updater {
 			'value'   => json_encode( $value )
 		);
 
-		update_option( $cache_key, $data );
+		update_option( $cache_key, $data, 'no' );
 
 	}
 
